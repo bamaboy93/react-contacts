466ebfea9bdc1e6b35d8acd24ac17bb4c123c3ae
Alexander Krupka <bamaboy483@gmail.com>
2023-02-05 11:24:15 +0200

    start files

---
+++ src/data/contacts.js
@@ -0,0 +1,6 @@
+export const contacts = [
+  { id: 1, number: 80934567823, name: "James Deen" },
+  { id: 2, number: 80734211223, name: "Sindee Wolf" },
+  { id: 3, number: 80675123456, name: "Vickie Row" },
+  { id: 4, number: 80669087641, name: "Jarred Smith" },
+];
